e31f570199146074fd4099557d51e3ec741c0cbc
FlipGoncalves <filipeg@ua.pt>
2022-04-13 18:57:52 +0100

    start implementation kafka in fotofaces

--- src/appFlask.py
+++ src/appFlask.py
@@ -9,6 +9,15 @@ import dlib
 import base64
 import json
 
+# kafka implementation
+import json
+import sys
+from random import choice
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from confluent_kafka import Producer, Consumer, OFFSET_BEGINNING
+
+
 app = Flask(__name__)
 
 
@@ -19,6 +28,19 @@ Reads two input images and an identifier:
 
 Outputs a dictionary with the cropped image (depending on some requirements), metrics and the same identifier.
 """
+
+# kafka implementation
+# Topic for producing messages
+TOPIC_PRODUCE = "image"
+# Topic for consuming messages
+TOPIC_CONSUME = "rev_image"
+
+# Set up a callback to handle the '--reset' flag.
+def reset_offset(consumer, partitions):
+    if args.reset:
+        for p in partitions:
+            p.offset = OFFSET_BEGINNING
+        consumer.assign(partitions)
 
 
 # Cropping threshold (for higher values the cropping might be bigger than the image itself
@@ -140,7 +162,39 @@ def upload_image():
         candidate = cv2.imdecode(
             np.frombuffer(base64.b64decode(img1), np.uint8), cv2.IMREAD_COLOR
         )
-
+        
+        
+        # # Kafka Implementation to message deal with the REST API
+        
+        # # GET photo from the database
+        # # produce a json message to send to the consumer
+        # producer.produce(TOPIC_PRODUCE, json.dumps({"command": "get_photo", "id": identifier}))
+        # producer.flush()
+    
+        # # Poll for new messages from Kafka and save the json object
+        # msg_json = None
+        # try:
+        #     while True:
+        #         msg = consumer.poll(1.0)
+        #         if msg is None:
+        #             pass
+        #         elif msg.error():
+        #             print(f"ERROR Recieving GET from the Database: {msg.error()}")
+        #         else:
+        #             msg_json = json.loads(msg.value().decode('utf-8'))
+        #             print(f"Consumed event from topic {TOPIC_CONSUME}: message = {msg_json}")
+        #             break
+        # except KeyboardInterrupt:
+        #     return False
+        
+        # # idk why it needs this but it doesn't work without it
+        # msg_json = json.loads(msg_json)
+        # # old photo from the database
+        # old_photo = msg_json["photo"]
+        # print(old_photo)
+        
+        
+        
         data = {}
         data["Colored Picture"] = is_gray(candidate)
         if data["Colored Picture"] == False:
@@ -207,4 +261,25 @@ plEngine = PluginEngine(options=
 
 
 if __name__ == "__main__":
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    parser.add_argument('--reset', action='store_true')
+    args = parser.parse_args()
+    
+    # Parse the configuration.
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+    
+    # Create Producer instance
+    producer = Producer(config)    
+
+    # Create Consumer instance
+    config.update(config_parser['consumer'])
+    consumer = Consumer(config)
+
+    consumer.subscribe([TOPIC_CONSUME], on_assign=reset_offset)
+    
     app.run(debug=True)
