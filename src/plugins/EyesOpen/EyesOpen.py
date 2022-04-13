ba1e89bcc62c778eaade971e7e1fb2d6fb6c0f90
FlipGoncalves <filipeg@ua.pt>
2022-04-13 18:39:24 +0100

    imp Eyes Open Plugin

---
+++ src/plugins/EyesOpen/EyesOpen.py
@@ -0,0 +1,27 @@
+from logging import Logger
+
+from engine import PluginCore
+from model import Meta
+
+import numpy as np
+import cv2
+
+class EyesOpen(PluginCore):
+
+    def __init__(self, logger: Logger) -> None:
+        super().__init__(logger)
+        self.meta = Meta(
+            name='Brightness plugin',
+            description='Plugin for Brightness level',
+            version='0.0.1'
+        )
+        
+    def invoke(self, args):
+        shape = args["shape"]
+        
+        leftEAR = (np.linalg.norm(shape[37]-shape[41]) + np.linalg.norm(shape[38]-shape[40]))/(2*np.linalg.norm(shape[36]-shape[39]))
+        rightEAR = (np.linalg.norm(shape[43]-shape[47]) + np.linalg.norm(shape[44]-shape[46]))/(2*np.linalg.norm(shape[42]-shape[45]))
+
+        avg = (leftEAR+rightEAR)/2
+        
+        return ("Eyes Open", avg)
