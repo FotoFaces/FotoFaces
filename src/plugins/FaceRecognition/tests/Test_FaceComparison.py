3a7309a39a26076abd8af7635584e76b897bcfb1
Vicente <vicente.costa@ua.pt>
2022-04-21 18:28:13 +0100

    comentarios and stuff

--- src/plugins/FaceRecognition/tests/Test_FaceComparison.py
+++ src/plugins/FaceRecognition/tests/Test_FaceComparison.py
@@ -3,7 +3,6 @@ import cv2
 import sys
 
 
-#######face detection
 
 def rect_to_bb(face):
 	x = face[3]
@@ -12,63 +11,46 @@ def rect_to_bb(face):
 	h = face[2] - y
 	return [x, y, w, h]
 
-""" imagePath = sys.argv[1]
-image = fc.load_image_file(imagePath)
-face_locations = fc.face_locations(image)
-
-
-print(face_locations)
-image = cv2.imread(imagePath)
-
-
-for face in face_locations:
-    [x, y ,w, h] = rect_to_bb(face)
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-cv2.imshow("first image", image)
-
-cv2.waitKey(0) """
 
 
 
 
 ###########     face comparison
 
+def testFaceRecognitionLib():
+    #load
+    imagePath1 = sys.argv[1]
+    imagePath2 = sys.argv[2]
 
-#load
-imagePath1 = sys.argv[1]
-imagePath2 = sys.argv[2]
-
-image1 = cv2.imread(imagePath1)
-image2 = cv2.imread(imagePath2)
-
-
-#comparison
-knownImage = fc.load_image_file(imagePath1)
-unknownImage = fc.load_image_file(imagePath2)
+    image1 = cv2.imread(imagePath1)
+    image2 = cv2.imread(imagePath2)
 
 
-known_encoding = fc.face_encodings(knownImage)[0]
-unknown_encoding = fc.face_encodings(unknownImage)[0]
-
-results = fc.compare_faces([known_encoding], unknown_encoding)
-
-
-#Draw rectanglens
+    #comparison
+    knownImage = fc.load_image_file(imagePath1)
+    unknownImage = fc.load_image_file(imagePath2)
 
 
-for img in [image1, image2]:
-    face_locations = fc.face_locations(img)
+    known_encoding = fc.face_encodings(knownImage)[0]
+    unknown_encoding = fc.face_encodings(unknownImage)[0]
 
-    for face in face_locations:
-        [x, y ,w, h] = rect_to_bb(face)
-        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    results = fc.compare_faces([known_encoding], unknown_encoding)
 
-    
-print(results)
-cv2.imshow("first image", image1)
-cv2.imshow("second image", image2)
 
-cv2.waitKey(0)
+    #Draw rectanglens
+
+
+    for img in [image1, image2]:
+        face_locations = fc.face_locations(img)
+
+        for face in face_locations:
+            [x, y ,w, h] = rect_to_bb(face)
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        
+    print(results)
+    cv2.imshow("first image", image1)
+    cv2.imshow("second image", image2)
+    cv2.waitKey(0)
 
 # https://face-recognition.readthedocs.io/en/latest/_modules/face_recognition/api.html#batch_face_locations
--- src/plugins/FaceRecognition/tests/Test_FaceDetection.py
+++ src/plugins/FaceRecognition/tests/Test_FaceDetection.py
@@ -9,12 +9,11 @@ import json
 import sys
 import fotofaces
 import time
-import face_recognition
+import face_recognition as fc
 
 
-#import FaceRecognition
 
-
+# (Assim por miudos) tradução de css para openCV 
 def rect_to_bb(face):
 	x = face[3]
 	y = face[0]
@@ -23,36 +22,43 @@ def rect_to_bb(face):
 	return [x, y, w, h]
 
 
-
+###########     Face_recognition library
 def testFaceRecognitionLib():
     print("Testing with Face_recognition library ....")
-    im = cv2.imread(sys.argv[1])
-    image = face_recognition.load_image_file(sys.argv[1])
+    im = cv2.imread(sys.argv[1]) #read image OpenCV para dps poder mexer nela
+
+    image = fc.load_image_file(sys.argv[1]) #read image pela fc algorithm
+    
     start = time.time()
-    face_locations = face_recognition.face_locations(image)
+    face_locations = fc.face_locations(image) # face detection algorithm
     duration = time.time() - start
 
     print(face_locations)
 
     for face in face_locations:
-        [x,y,w,h] = rect_to_bb(list(face))
-        cv2.rectangle(im, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        [x,y,w,h] = rect_to_bb(list(face)) # conversão (só aceita ¯\_(ツ)_/¯ )
+        cv2.rectangle(im, (x, y), (x+w, y+h), (0, 255, 0), 2) # desenhar retangulos
+    
     cv2.imshow("Faces found", im)
     cv2.waitKey(0)
     print("Duration: "+ str(duration))
     print("End of Testing with OpenCV")
 
 
+###########     Using OpenCV library
 
 def testOpenCVDetection():
     print("Testing with OpenCV ....")
+    
     start = time.time()
-    image = OpenCVDetection(sys.argv[1], sys.argv[2])
+    image = OpenCVDetection(sys.argv[1], sys.argv[2]) 
     duration = time.time() - start
+    
     height, width = image.shape[:2]
     res = cv2.resize(image,(width//4, height//4), interpolation = cv2.INTER_AREA)
     cv2.imshow("Faces found", res)
     cv2.waitKey(0)
+    
     print("Duration: "+ str(duration))
     print("End of Testing with OpenCV")
 
@@ -60,39 +66,54 @@ def testOpenCVDetection():
 
 
 def OpenCVDetection(imagePath, cascPath):
-    faceCascade = cv2.CascadeClassifier(cascPath)
+    faceCascade = cv2.CascadeClassifier(cascPath) #cena para detetar caras (tbm n sei muito os detalhes)
+    
     image = cv2.imread(imagePath)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #conversão para escala de cinzentos
+
+    faces = faceCascade.detectMultiScale(   #algoritmo de detetação
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
         flags = cv2.CASCADE_SCALE_IMAGE
-    )
-    for (x, y, w, h) in faces:
+    ) 
+
+    for (x, y, w, h) in faces: #desenhar caras
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
     return image
 
 
+###########     Using Old Fotofaces algorithm 
 
 def testOldDetection():
     print("Testing with Old Fotofaces Algorithm ....")
+    
     start = time.time()
     image = OldDetection(sys.argv[1])
     duration = time.time() - start
+
     height, width = image.shape[:2]
     res = cv2.resize(image,(width//4, height//4), interpolation = cv2.INTER_AREA)
     cv2.waitKey(0)
+    
     print("Duration: "+ str(duration))
     print("End of Testing with Old Fotofaces Algorithm")
 
 def OldDetection(imagePath):
+
     image = cv2.imread(imagePath)
+
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
     max_shape, max_bb, raw_shape = fotofaces.detect_face(gray)
+    #desenhar retangulos 
     cv2.rectangle(image, (max_bb[0], max_bb[1]), (max_bb[0]+max_bb[2], max_bb[1]+max_bb[3]), (0, 255, 0), 2)
     return image
+
+
+
+###########     main 
 
 testOpenCVDetection()
 print("\n\n")
