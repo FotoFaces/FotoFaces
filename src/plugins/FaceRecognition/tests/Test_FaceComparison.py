146308f8a3f4bb6f63f4975e91cf85989ed40851
Vicente <vicente.costa@ua.pt>
2022-04-21 14:37:45 +0100

    test face comparison and detection

--- src/plugins/FaceRecognition/tests/Test_FaceComparison.py
+++ src/plugins/FaceRecognition/tests/Test_FaceComparison.py
@@ -2,7 +2,17 @@ import face_recognition as fc
 import cv2
 import sys
 
-imagePath = sys.argv[1]
+
+#######face detection
+
+def rect_to_bb(face):
+	x = face[3]
+	y = face[0]
+	w = face[1] - x
+	h = face[2] - y
+	return [x, y, w, h]
+
+""" imagePath = sys.argv[1]
 image = fc.load_image_file(imagePath)
 face_locations = fc.face_locations(image)
 
@@ -10,14 +20,55 @@ face_locations = fc.face_locations(image)
 print(face_locations)
 image = cv2.imread(imagePath)
 
-for (x, y, w, h) in face_locations:
+
+for face in face_locations:
+    [x, y ,w, h] = rect_to_bb(face)
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv2.imshow("Faces found", image)
+cv2.imshow("first image", image)
+
+cv2.waitKey(0) """
+
+
+
+
+###########     face comparison
+
+
+#load
+imagePath1 = sys.argv[1]
+imagePath2 = sys.argv[2]
+
+image1 = cv2.imread(imagePath1)
+image2 = cv2.imread(imagePath2)
+
+
+#comparison
+knownImage = fc.load_image_file(imagePath1)
+unknownImage = fc.load_image_file(imagePath2)
+
+
+known_encoding = fc.face_encodings(knownImage)[0]
+unknown_encoding = fc.face_encodings(unknownImage)[0]
+
+results = fc.compare_faces([known_encoding], unknown_encoding)
+
+
+#Draw rectanglens
+
+
+for img in [image1, image2]:
+    face_locations = fc.face_locations(img)
+
+    for face in face_locations:
+        [x, y ,w, h] = rect_to_bb(face)
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    
+print(results)
+cv2.imshow("first image", image1)
+cv2.imshow("second image", image2)
+
 cv2.waitKey(0)
-
-
-
-
 
 # https://face-recognition.readthedocs.io/en/latest/_modules/face_recognition/api.html#batch_face_locations
--- src/plugins/FaceRecognition/tests/Test_FaceDetection.py
+++ src/plugins/FaceRecognition/tests/Test_FaceDetection.py
@@ -12,57 +12,61 @@ import time
 #import FaceRecognition
 
 
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]  
-faceCascade = cv2.CascadeClassifier(cascPath)
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# faces = faceCascade.detectMultiScale(
-#     gray,
-#     scaleFactor=1.1,
-#     minNeighbors=5,
-#     minSize=(30, 30),
-#     flags = cv2.CASCADE_SCALE_IMAGE
-# )
-
-# print("Found {0} faces!".format(len(faces)))
-start = time.time()
-max_shape, max_bb, raw_shape = fotofaces.detect_face(gray)
-end = time.time() - start
-# Draw a rectangle around the faces
-# for (x, y, w, h) in faces:
-#     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-cv2.rectangle(image, (max_bb[0], max_bb[1]), (max_bb[0]+max_bb[2], max_bb[1]+max_bb[3]), (0, 255, 0), 2)
-
-height, width = image.shape[:2]
-print("Do gajo")
-print(end)
-
-start = time.time()
-# faces = faceCascade.detectMultiScale(
-#     gray,
-#     scaleFactor=1.1,
-#     minNeighbors=5,
-#     minSize=(30, 30),
-#     flags = cv2.CASCADE_SCALE_IMAGE
-# )
-end = time.time() - start
-# for (x, y, w, h) in faces:
-#     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-print("Nosso")
-#print("Found {0} faces!".format(len(faces)))
-
-print(end)
 
 
-#print(height//4)
-#print(width//4)
-
-res = cv2.resize(image,(width//4, height//4), interpolation = cv2.INTER_AREA)
-cv2.imshow("Faces found", res)
-cv2.waitKey(0)
 
 
-    
+
+def testOpenCVDetection():
+    print("Testing with OpenCV ....")
+    start = time.time()
+    image = OpenCVDetection(sys.argv[1], sys.argv[2])
+    duration = time.time() - start
+    height, width = image.shape[:2]
+    res = cv2.resize(image,(width//4, height//4), interpolation = cv2.INTER_AREA)
+    cv2.imshow("Faces found", res)
+    cv2.waitKey(0)
+    print("Duration: "+ str(duration))
+    print("End of Testing with OpenCV")
+
+
+
+
+def OpenCVDetection(imagePath, cascPath):
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    return image
+
+
+
+def testOldDetection():
+    print("Testing with Old Fotofaces Algorithm ....")
+    start = time.time()
+    image = OldDetection(sys.argv[1])
+    duration = time.time() - start
+    height, width = image.shape[:2]
+    res = cv2.resize(image,(width//4, height//4), interpolation = cv2.INTER_AREA)
+    cv2.waitKey(0)
+    print("Duration: "+ str(duration))
+    print("End of Testing with Old Fotofaces Algorithm")
+
+def OldDetection(imagePath):
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    max_shape, max_bb, raw_shape = fotofaces.detect_face(gray)
+    cv2.rectangle(image, (max_bb[0], max_bb[1]), (max_bb[0]+max_bb[2], max_bb[1]+max_bb[3]), (0, 255, 0), 2)
+    return image
+
+testOpenCVDetection()
+print("\n\n")
+testOldDetection()
