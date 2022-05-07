bb0bd73189c9f10fe90facb48c9d4ecdb9ba15c0
FlipGoncalves <filipeg@ua.pt>
2022-05-07 16:36:41 +0100

    REST API

--- APIs/RESTApi.py
+++ APIs/RESTApi.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 import sqlite3 as sql
 import logging
+from flask_cors import CORS, cross_origin
+import hashlib
 
 # logger
 logging.basicConfig(filename="api.log", level=logging.DEBUG)
@@ -12,6 +14,7 @@ logging.basicConfig(filename="api.log", level=logging.DEBUG)
 app = Flask(__name__)
 app.logger.debug("\n\n\n\t\tFlask app start")
 api = Api(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 # mock database created with sqlite3
@@ -21,7 +24,7 @@ with sql.connect('mydb') as con:
         id int primary key,
         email text,
         full_name text,
-        password varchar,
+        password text,
         photo blob);""")
     cur.close()
 
@@ -29,7 +32,39 @@ with sql.connect('mydb') as con:
 # get all users with all the information
 # debug material
 @app.route("/")
+@cross_origin()
 def index():
+    
+    ### INSERT DATABASE
+    
+    # password = hashlib.md5(("adminpg").encode("utf-8", errors='static')).hexdigest()
+
+    # with open("GoncaloOldFoto.jpg", "rb") as f:
+    #     ref_bytes = f.read()
+    #     goncas = base64.b64encode(ref_bytes).decode("utf8")
+        
+    # with open("vicente.png", "rb") as f:
+    #     ref_bytes = f.read()
+    #     vicente = base64.b64encode(ref_bytes).decode("utf8")
+        
+    # with open("borges.png", "rb") as f:
+    #     ref_bytes = f.read()
+    #     borges = base64.b64encode(ref_bytes).decode("utf8")
+    
+    # with open("flips.jpg", "rb") as f:
+    #     ref_bytes = f.read()
+    #     fip = base64.b64encode(ref_bytes).decode("utf8")
+    
+    # with sql.connect('mydb') as con:
+    #     cur = con.cursor()
+    #     cur.execute("INSERT INTO User VALUES(?,?,?,?,?)", (98083, "filipe@ua.pt", "Filipe", password, fip))
+    #     cur.execute("INSERT INTO User VALUES(?,?,?,?,?)", (97827, "mini@ua.pt", "Mini", password, "photo"))
+    #     cur.execute("INSERT INTO User VALUES(?,?,?,?,?)", (98515, "vicente@ua.pt", "Vicente", password, vicente))
+    #     cur.execute("INSERT INTO User VALUES(?,?,?,?,?)", (98155, "borges@ua.pt", "Borges", password, borges))
+    #     cur.execute("INSERT INTO User VALUES(?,?,?,?,?)", (98359, "goncas@ua.pt", "Maestro", password, goncas))
+    #     con.commit()
+    #     cur.close()
+    
 
     # database call
     app.logger.debug("-- Begin -- Database call in index")
@@ -128,12 +163,60 @@ class Image(Resource):
 # Class generalization for the CRUD methods related to the User -> database connection
 # Mock class for testing
 class User(Resource):
+    
+    # GET method
+    # input arguments: identification (id or something similar) -> NMEC if possible
+    # get the photo in the database for the user with id inputed
+    def get(self, param):
+        # curl http://localhost:5000/image/<id> -X GET
+
+
+        message = "adminpg"
+        message_bytes = message.encode('ascii')
+        base64_bytes = base64.b64encode(message_bytes)
+        base64_message = base64_bytes.decode('ascii')
+
+        print(base64_message)
+
+
+        name = param
+
+        # database call
+        app.logger.debug("-- Begin -- Database call while getting the old photo")
+        try:
+            with sql.connect('mydb') as con:
+                cur = con.cursor()
+                # SELECT * FROM <Table of Users> WHERE <identification> = <inputed identification>
+                cur.execute(f"SELECT id, photo, password FROM User WHERE email = \'{name}\'")
+                result = cur.fetchall()
+                cur.close()
+                # in case we dont get any results
+                if result == []:
+                    # debug
+                    app.logger.debug(f"{result}")
+                    return jsonify({"command": "get_user", "email": name, "error": "result is empty"})
+                app.logger.debug(f"Successful query")
+        # in case we get an unexpected error
+        except KeyError as k:
+            app.logger.debug(f"Error: {k}")
+            return jsonify({"command": "get_user", "email": name, "error": k})
+
+        app.logger.debug("-- End -- Database call while getting the old photo")
+
+        # extract photo from the list of results
+        # result len should be 1 and only 1
+        (identifier, photo, password) = result[0]
+
+        # return identification and photo
+        return jsonify({"command": "get_user", "email": name, "photo": photo, "password": password, "id": identifier})
 
     # PUT method
     # input arguments: identification (id or something similar) -> NMEC if possible
     # updates / inserts a user to the database
-    def put(self, identification):
-        # curl http://localhost:5000/add_user/<id> -d "email=<email>" -d "name=<name>" -d "password=<password>" -d "photo=<photo>" -X PUT
+    def put(self, param):
+        # curl http://localhost:8393/user/<id> -d "email=<email>" -d "name=<name>" -d "password=<password>" -d "photo=<photo>" -X PUT
+
+        identification = int(param)
 
         # get variables necessary of a user
         photo = request.form['photo']
@@ -172,13 +255,13 @@ api.add_resource(Image, '/image/<int:identification>')
 
 # add User class to the url so the methods from User class can get called
 # <int:identification> -> identification for the method
-api.add_resource(User, '/add_user/<int:identification>')
+api.add_resource(User, '/user/<param>')
 
 
 # main
 if __name__ == "__main__":
     # run Flask app
-    app.run(host="0.0.0.0", port=8393)
+    app.run(host="0.0.0.0", port=8393, debug=True)
 
 
 
