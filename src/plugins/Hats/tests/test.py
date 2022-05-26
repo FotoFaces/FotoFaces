d620e053dcf2e864a600f0f87b9f3694cf805a2f
Vicente <vicente.costa@ua.pt>
2022-05-26 17:15:00 +0100

    ready to test

--- src/plugins/Hats/tests/test.py
+++ src/plugins/Hats/tests/test.py
@@ -4,6 +4,53 @@ import numpy as np
 import sys
 import dlib
 
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+def bb_area( bb):
+        return (bb[0] + bb[2]) * (bb[1] + bb[3])
+
+# Converts dlib format to numpy format
+def shape_to_np( shape):
+    landmarks = np.zeros((68, 2), dtype=int)
+
+    for i in range(0, 68):
+        landmarks[i] = (shape.part(i).x, shape.part(i).y)
+
+    return landmarks
+
+# Converts dlib format to opencv format
+def rect_to_bb( rect):
+    x = rect.left()
+    y = rect.top()
+    w = rect.right() - x
+    h = rect.bottom() - y
+    return [x, y, w, h]
+
+def detect_face(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray_image, 1)
+    max_area, max_bb, max_shape, raw_shape = (0, None, None, None)
+
+    for (z, rect) in enumerate(rects):
+        if (
+            rect is not None
+            and rect.top() >= 0
+            and rect.right() < gray_image.shape[1]
+            and rect.bottom() < gray_image.shape[0]
+            and rect.left() >= 0
+        ):
+            predicted = predictor(gray_image, rect)
+            bb = rect_to_bb(rect)
+            area = bb_area(bb)
+            # only returns the largest bounding box to avoid smaller false positives
+            if area > max_area:
+                max_area = area
+                max_bb = bb
+                max_shape = shape_to_np(predicted)
+                raw_shape = predicted
+
+    return max_shape, max_bb, raw_shape
 
 
 # Detects sunglasses using  Machine Learning trained model
