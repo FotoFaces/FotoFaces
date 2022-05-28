944a6b07f88256aeeb39f8555e58f4468d912d15
Vicente <vicente.costa@ua.pt>
2022-05-28 15:50:45 +0100

    merge

--- src/plugins/FaceRecognition/FaceRecognition.py
+++ src/plugins/FaceRecognition/FaceRecognition.py
@@ -17,8 +17,6 @@ class FaceRecognition(PluginCore):
             version='0.0.1'
         )
         self.facerec = dlib.face_recognition_model_v1("dlib_face_recognition_resnet_model_v1.dat")
-        self.detector = dlib.get_frontal_face_detector()
-        self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
         self.appCore = appCore
 
 
@@ -31,21 +29,25 @@ class FaceRecognition(PluginCore):
         """
 
         #ficheiro para comparação de caras
-
-        photo = args.get("candidate")
-        self._logger.info(args.keys())
         reference = args["reference"]
-        self._logger.info(photo[:30])
+        candidate = args["candidate"]
+
+        self._logger.info(args.keys())
+        self._logger.info(candidate[:30])
         self._logger.info(reference[:30])
 
 
+
+
+        photo_raw_shape = args["raw_shape"]
+
         #deteção de uma cara nas fotos
-        reference_raw_shape = self.detect_face(reference)[2]
-        photo_raw_shape = self.detect_face(photo)[2]
+        reference_raw_shape = self.appCore.detect_face(reference)[2]
+
 
         #buscar o chip da cara
         reference_chip = dlib.get_face_chip(reference, reference_raw_shape)
-        photo_chip = dlib.get_face_chip(photo, photo_raw_shape)
+        photo_chip = dlib.get_face_chip(candidate, photo_raw_shape)
 
         #usar face recognition file
         reference_descriptor = np.asarray(self.facerec.compute_face_descriptor(reference_chip))
@@ -54,38 +56,3 @@ class FaceRecognition(PluginCore):
         #resultado da comparação se for inferior a 0.6 são a mesma pessoa
         tolerance = np.linalg.norm(reference_descriptor - photo_descriptor)
         return ("Face Recognition", tolerance)
-
-    def detect_face(self, img):
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        rects = self.detector(gray_img, 1)
-        max_area, max_bb, max_shape, raw_shape = (0, None, None, None)
-        for (z, rect) in enumerate(rects):
-            if rect is not None and rect.top() >= 0 and rect.right() < gray_img.shape[1] and rect.bottom() < gray_img.shape[0] and rect.left() >= 0:
-                predicted = self.predictor(gray_img, rect)
-                bb = self.rect_to_bb(rect)
-                area = self.bb_area(bb)
-                # only returns the largest bounding box to avoid smaller false positives
-                if area > max_area:
-                    max_area = area
-                    max_bb = bb
-                    max_shape = self.shape_to_np(predicted)
-                    raw_shape = predicted
-        return max_shape, max_bb, raw_shape
-
-    def rect_to_bb(self,rect):
-        x = rect.left()
-        y = rect.top()
-        w = rect.right() - x
-        h = rect.bottom() - y
-        return [x, y, w, h]
-
-    def bb_area(self,bb):
-        return (bb[0]+bb[2])*(bb[1]+bb[3])
-
-    def shape_to_np(self,shape):
-        landmarks = np.zeros((68,2), dtype = int)
-
-        for i in range(0,68):
-            landmarks[i] = (shape.part(i).x, shape.part(i).y)
-
-        return landmarks
---
+++ src/plugins/Hats/HatDetection.py
@@ -0,0 +1,86 @@
+from logging import Logger
+
+from engine import PluginCore
+from model import Meta
+
+import numpy as np
+import cv2
+
+class HatDetection(PluginCore):
+
+    def __init__(self, logger: Logger, appCore) -> None:
+        super().__init__(logger, appCore)
+        self.meta = Meta(
+            name='HatDetection plugin',
+            description='Plugin to detect if a person is wearing a hat',
+            version='0.0.1'
+        )
+        self.appCore = appCore
+        self.net = cv2.dnn.readNet("deploy.prototxt", "hed_pretrained_bsds.caffemodel")
+    # Detects sunglasses using  Machine Learning trained model
+
+    def invoke(self, args):
+
+        candidate = args["final_img"]
+        shape = args["shape"]
+        
+        roi_cropp = cropping_hats(candidate, shape)
+        #print("roi_cropp",roi_cropp)
+        img_size = 150
+        resized_arr = cv2.resize(roi_cropp, (img_size, img_size)) # Reshaping images to preferred size
+        resized_arr = cv2.cvtColor(resized_arr, cv2.COLOR_RGB2BGR)
+
+        x_test = []
+        x_test.append(resized_arr)
+        x_test = np.array(x_test)
+        x_test = (x_test / 127.5 ) - 1
+
+        # load json and create model
+        json_file = open('../model_tf_hats_head_cropp_hand_final.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights("../model_tf_hats_head_cropp_hand_final.h5")
+        print("Loaded model from disk")
+
+        # evaluate loaded model on test data
+        loaded_model.compile(optimizer=tf.keras.optimizers.Adam(1e-5),  # Very low learning rate
+                    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+                    metrics=[keras.metrics.BinaryAccuracy()])
+
+        predictions = (loaded_model.predict(np.array(x_test)) > 0.5).astype("int32")
+
+        #print("Prediction Hats: ", predictions[0])
+
+        if predictions[0] == 1:
+            return ('Hats' , "true")
+
+        return ('Hats' , "false")
+
+
+    # Crops the image into the head reagion to give as input to the hats trained model 
+    def cropping_hats(image, shape):
+
+        CROP_ALPHA = 0.75
+
+        aux = shape[0] - shape[16]
+        distance = np.linalg.norm(aux)
+        h = int(distance)
+
+        middle_X = int((shape[0][0] + shape[16][0])/2)
+        middle_Y = int((shape[19][1] + shape[29][1])/2)
+
+        x1 = int((shape[0][0]))
+        y1 = int(middle_Y-h*CROP_ALPHA)
+        x2 = int((shape[16][0]))
+        y2 = int((shape[19][1]))
+        tl = (x1, y1)
+        br = (x2, y2)
+
+        if x1 >= 0 and y1 >= 0 and x2 < image.shape[1] and y2 < image.shape[0]:
+            roi = image[tl[1]:br[1],tl[0]:br[0]]
+            return roi
+
+        else: 
+            return None
